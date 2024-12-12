7c487718de49b374b2232c65a3bbe5328da899d1
Beau Harrison <beauharrison@gmail.com>
2024-12-12 04:33:55 +0000

    :bug: Read from the right tables.

--- day01/solve.sql
+++ day01/solve.sql
@@ -23,8 +23,8 @@ differences AS (
         sorted_col1.col1,
         sorted_col2.col2,
         sorted_col2.col2 - sorted_col1.col1 AS difference
-    FROM sorted_data
-    JOIN sorted_col1 ON sorted_col1.rn = sorted_col2.rn
+    FROM sorted_col1
+    JOIN sorted_col2 ON sorted_col1.rn = sorted_col2.rn
 )
 SELECT 
     SUM(difference) AS solution,
